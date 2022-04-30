06512f463d6d118be5816547a83e85947a0a6c8b
Prof. Dr. Claudius Stern <stern@biozoom.net>
2022-04-30 15:43:17 +0200

    Connect to WiFi and MQTT broker

--- src/firmware.cpp
+++ src/firmware.cpp
@@ -11,5 +11,13 @@ void Firmware::begin(String mqttServer) {
     this->wifiName = WIFI_NAME;
     this->mqttServer = mqttServer;
     // Connect to WiFi
+    WiFi.begin(WIFI_NAME, WIFI_PASS);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(100); // 100ms warten...
+    }
     // Connect to MQTT broker
+    while (!mqttClient.connect(CLIENT_ID, "public", "public")) {
+        // Serial.print(".");
+        delay(1000);
+    }
 }
